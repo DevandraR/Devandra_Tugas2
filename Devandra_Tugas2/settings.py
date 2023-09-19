ab539d71c18f3a5cc3388b6c2cbe19676d207f95
Devandra <devandra271203@gmail.com>
2023-09-19 10:00:59 +0700

    Tugas 3 Finishing

--- Devandra_Tugas2/settings.py
+++ Devandra_Tugas2/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Devandra_Tugas2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
