5df50e842090ca4151fe9f2cd2233a4090ee4074
Devandra <devandra271203@gmail.com>
2023-11-22 10:33:50 +0700

    Tugas 9 Bug Fix #2

--- Devandra_Tugas2/settings.py
+++ Devandra_Tugas2/settings.py
@@ -135,6 +135,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ["https://devandra-reswara-tugas.pbp.cs.ui.ac.id"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
